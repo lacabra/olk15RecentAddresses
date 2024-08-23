1aded69adaa00a0e8bc2ad4ebf872cb8feffc0b4
Victor Grau Serrat <lacabra@users.noreply.github.com>
2024-08-23 16:06:45 +0200

    BUG: first and last names encoded with UTF-16LE

--- RecentAddressesParser/parser.py
+++ RecentAddressesParser/parser.py
@@ -1,5 +1,4 @@
 import binascii
-import struct
 import itertools
 import logging
 
@@ -16,10 +15,13 @@ class Parser(object):
             logging.basicConfig(level=logging.DEBUG)
         else:
             logging.basicConfig(level=logging.INFO)
-
         self.logger = logging.getLogger(self.__class__.__name__)
-        self.filename = filename
-        self.start_of_email_index = 66  # 66th byte is the 132 character
+
+        self.hex_list = []
+        with open(filename, "rb") as fp:
+            #  Convert each byte of the file into its hexadecimal representation
+            #  and store these in a list.
+            self.hex_list = ["{:02x}".format(c) for c in fp.read()]
 
     # https://docs.python.org/3.1/library/itertools.html  # recipes
     def grouper(self, n, iterable, fillvalue=None):
@@ -27,64 +29,78 @@ class Parser(object):
         args = [iter(iterable)] * n
         return itertools.zip_longest(*args, fillvalue=fillvalue)
 
-    def find_section_boundaries(self, hex_list, start):
-        subsection_start = 0
+    def little_endian(self, byte_array):
+        lsb = int(byte_array[0], 16)  # Least Significant Byte
+        msb = int(byte_array[1], 16)  # Most Significant Byte
+        # Combine the bytes in little-endian order to form the 16-bit integer
+        return (msb << 8) | lsb
+
+    def decode_header(self):
+        # Number of sections in the file, this always should be 4
+        self.num_sections = self.little_endian(self.hex_list[44:46])
+        self.logger.debug(f"Number of sections: {self.num_sections}")
+        # We'll compute all section boundaries in absoulte numbers
+        # They are all encoded as relative offsets from the previous section
+        # using 16-bit little-endian integers. Each section has a 2-byte '0000'
+        # delimiter that is already accounted for in the offsets.
+        self.section_emails = {
+            "start": 66,  # This is hardcoded, start of section after file header
+            "lengths": self.little_endian(self.hex_list[46:48]) + 66,
+        }
+        self.logger.debug(f"Email Section boundaries: {self.section_emails}")
+
+        start_offset = self.little_endian(self.hex_list[48:50])
+        length_offset = self.little_endian(self.hex_list[50:52])
+        self.section_firstnames = {
+            "start": self.section_emails["lengths"] + start_offset,
+            "lengths": self.section_emails["lengths"] + start_offset + length_offset,
+        }
+        self.logger.debug(f"Firstname Section boundaries: {self.section_firstnames}")
+
+        start_offset = self.little_endian(self.hex_list[52:54])
+        length_offset = self.little_endian(self.hex_list[54:56])
+        self.section_lastnames = {
+            "start": self.section_firstnames["lengths"] + start_offset,
+            "lengths": self.section_firstnames["lengths"]
+            + start_offset
+            + length_offset,
+        }
+        self.logger.debug(f"Lastname Section boundaries: {self.section_lastnames}")
+
+        # There is an additional 4th section, but I haven't found any meaningful
+        # information there, so we are skipping it altogether for now
+
+        # The number of field lengths gives us the number of fields
+        # (encoded in 4 bytes, so we divide by 4 to obtain the actual number)
+        self.num_emails = int(self.little_endian(self.hex_list[48:50]) / 4) - 1
+        self.logger.debug(f"Number of emails: {self.num_emails}")
+
+    def find_indices(self, section):
         # Initialize this section's start index
         section_indices = [
-            start,
+            section["start"],
         ]
 
-        for i, byte in enumerate(hex_list):
-            if i < start:
-                continue
-            if subsection_start == 0 and byte == "00" and hex_list[i + 1] == "00":
-                subsection_start = i + 2  # This is where the subsection begins
+        # compute the start and end of the indices section. The section starts
+        # with a 2-byte '0000' delimiter, hence we add 2 at the start. The length
+        # is the number of fields (emails) times 4 bytes per field length
+        start = section["lengths"] + 2
+        end = start + self.num_emails * 4
 
-        self.logger.debug(f"Subsection start: {subsection_start}")
-
-        # The subsection start must be an even number, multiple of 2
-        # if it's an odd number, we are one byte short, so we add one more
-        subsection_start = subsection_start + (subsection_start % 2)
-
-        # group bytes in groups of four, padd with '00' if needed
-        four_byte_groups = self.grouper(4, hex_list[subsection_start:], "00")
-
-        # Keep track of how far we travel in these groups of four, as this will
-        # mark the end of the section
-        index = 0
+        # Group 4 bytes at a time
+        four_byte_groups = self.grouper(4, self.hex_list[start:end], "00")
 
         for group in four_byte_groups:
-            # for each group of four, get the second two bytes and int them
-            hex_rep = binascii.unhexlify("".join(group[2:]))
-            try:
-                # They should have an integer value with the length of each item
-                # in this section (email, firstname or lastname). If the integer
-                # conversion fails, it means our assumptions about the encoding
-                # of this file are wrong, and need to be revised
-                int_val = struct.unpack("<h", hex_rep)[0]
-            except Exception as e:
-                print("Error processing group %s: %s" % (hex_rep, e))
-                int_val = -1
-                continue
+            # The length of each field is encoded with 16-bit little-endian integers
+            # using the last 2 bytes
+            offset = self.little_endian(group[2:])
 
-            # their value is the next thing to add to section_indeces,
-            # which are all referenced to the beginning of the file
-            section_indices.append(start + int_val)
+            # The length of each field is computed as an offset from the start of each section
+            section_indices.append(section["start"] + offset)
 
-            # advance the index by 4
-            index += 4
+        return section_indices
 
-            # once their value is equal to or greater than subsection_start,
-            # stop processing because we reached the end of the section
-            if int_val + start >= subsection_start - 2:
-                break
-
-        # Calculate where the next section starts:
-        next_start = section_indices[-1] + index + 4
-
-        return section_indices, next_start
-
-    def decode_section(self, hex_list, section_indices):
+    def decode_section(self, section_indices, utf16=False):
         # We now know that our email address go from byte self.start_of_email
         # to self.end_of_email
         section_hex = []
@@ -92,42 +108,30 @@ class Parser(object):
             self.logger.debug(f"{i} {index}")
             if i == len(section_indices) - 1:
                 break
-            section_hex.append(hex_list[index : section_indices[i + 1]])
+            section_hex.append(self.hex_list[index : section_indices[i + 1]])
 
+        # The email section is encoded using UTF-8 whereas the first and last name
+        # sections are encoded using UTF-16LE (Little-Endian)
+        encoding = "utf-16le" if utf16 else "utf-8"
         contents = [
-            binascii.unhexlify("".join(item)).decode("utf-8").replace("\x00", "")
-            for item in section_hex
+            binascii.unhexlify("".join(item)).decode(encoding) for item in section_hex
         ]
 
         return contents
 
     def go(self):
 
-        #  Open the file in binary mode for reading
-        with open(self.filename, "rb") as fp:
-            #  Convert each byte of the file into its hexadecimal representation
-            #  and store these in a list.
-            hex_list = ["{:02x}".format(c) for c in fp.read()]
+        self.decode_header()
 
-            # Find each of the three section boundaries, and the indices of each
-            # item in each section: email, firstname, lastname
-            email_indices, start_next_section = self.find_section_boundaries(
-                hex_list, self.start_of_email_index
-            )
-            self.logger.debug(f"{email_indices} {start_next_section}")
-            firstname_indices, start_next_section = self.find_section_boundaries(
-                hex_list, start_next_section
-            )
-            self.logger.debug(f"{firstname_indices} {start_next_section}")
-            lastname_indices, start_next_section = self.find_section_boundaries(
-                hex_list, start_next_section
-            )
-            self.logger.debug(f"{lastname_indices} {start_next_section}")
+        # Find the indices in each section: email, firstname, lastname
+        email_indices = self.find_indices(self.section_emails)
+        firstname_indices = self.find_indices(self.section_firstnames)
+        lastname_indices = self.find_indices(self.section_lastnames)
 
-            # Decode each section
-            emails = self.decode_section(hex_list, email_indices)
-            firstnames = self.decode_section(hex_list, firstname_indices)
-            lastnames = self.decode_section(hex_list, lastname_indices)
+        # Decode each section
+        emails = self.decode_section(email_indices, False)
+        firstnames = self.decode_section(firstname_indices, True)
+        lastnames = self.decode_section(lastname_indices, True)
 
-            # Aggregate iterables into tuples, and convert them into a list
-            return list(zip(emails, firstnames, lastnames))
+        # Aggregate iterables into tuples, and convert them into a list
+        return list(zip(emails, firstnames, lastnames))
